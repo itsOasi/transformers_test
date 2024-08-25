f1667ebd25be479005cb699b871db7dc2698b3a0
itsOasi <ostally001@gmail.com>
2024-08-25 01:00:05 -0700

    update main.py

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ flask_app = Flask(__name__)
 ASSET_DIR = "" # where repo data will be stored
 ADMIN_PASSWD = "1234" # get from environment variable
 
-@app.route("/", methods=["GET", "POST"]) # loads homepage
+@flask_app.route("/", methods=["GET", "POST"]) # loads homepage
 def main():
 	if request.method == "POST":
 		message = request.form["message"]
