59a9bcacaa53581ea7e44fc9707d3b9588b08217
itsOasi <ostally001@gmail.com>
2024-08-25 01:39:30 -0700

    added more pipelines

--- src/app.py
+++ src/app.py
@@ -1,12 +1,21 @@
 from transformers import pipeline
 
-def get_sentiment(text):
+def get_sentiment(prompt):
 	classifier = pipeline("sentiment-analysis")
-	res = classifier(text)
+	res = classifier(prompt)
+	return res
+
+def get_generated(prompt):
+	generator = pipeline("text-generation")
+	res = generator(prompt)
+	return res
+
+def get_answer(prompt):
+	answer = pipeline("question-answering")
+	res = answer(prompt)
 	return res
 
 if __name__ == "__main__":
 	classifier = pipeline("sentiment-analysis")
 	res = classifier("I've been waiting for a HuggingFace course my whole life.")
-
 	print(res)
--- src/main.py
+++ src/main.py
@@ -9,11 +9,28 @@ ADMIN_PASSWD = "1234" # get from environment variable
 @flask_app.route("/", methods=["GET", "POST"]) # loads homepage
 def main():
 	if request.method == "POST":
-		message = request.form["message"]
-		res = app.get_sentiment(message)
+		message = request.form["prompt"]
+		res = app.get_generated(message)
 		return render_template("result.html", result=res)
 	return render_template("index.html")
 
+flask_app.route("analyze", methods=["POST"])
+def analyze():
+	message = request.form["prompt"]
+	res = app.get_sentiment(message)
+	return render_template("result.html", result=res)
+
+flask_app.route("generate", methods=["POST"])
+def generate():
+	message = request.form["prompt"]
+	res = app.get_generated(message)
+	return render_template("result.html", result=res)
+
+flask_app.route("answer", methods=["POST"])
+def sentiment():
+	message = request.form["prompt"]
+	res = app.get_sentiment(message)
+	return render_template("result.html", result=res)
 
 
 if __name__ == "__main__":
